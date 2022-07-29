86e6ec35abe31135d9fb5e8bc2edda2956fcd1c2
Manoel Iranilson <manoelvieirafilho0@gmail.com>
2022-07-29 18:08:46 -0300

    Page Step3

--- src/pages/FormStep03/index.tsx
+++ src/pages/FormStep03/index.tsx
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { ChangeEvent, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { FormActions, useForm } from "../../contexts/FormContext";
 
 import * as S from "./styles";
 
 const FormStep02: React.FC = () => {
-  return <S.Container>Passo3</S.Container>;
+  const navigate = useNavigate();
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    if (state.name === "") {
+      navigate("/");
+    } else {
+      dispatch({
+        type: FormActions.setCurrentStep,
+        payload: 2,
+      });
+    }
+  });
+
+  function EmailChange(e: ChangeEvent<HTMLInputElement>) {
+    dispatch({
+      type: FormActions.setEmail,
+      payload: e.target.value,
+    });
+  }
+
+  function GitChange(e: ChangeEvent<HTMLInputElement>) {
+    dispatch({
+      type: FormActions.setGitHub,
+      payload: e.target.value,
+    });
+  }
+
+  function LinkedinChange(e: ChangeEvent<HTMLInputElement>) {
+    dispatch({
+      type: FormActions.setLinkedin,
+      payload: e.target.value,
+    });
+  }
+
+  function Finish() {
+    if (state.github !== "" && state.email !== "") {
+      console.log(state);
+    } else {
+      alert("Preencha todos os dados!!");
+    }
+  }
+  return (
+    <S.Container>
+      <p>Passo {state.currentStep}/3</p>
+      <h1>{state.name}, Onde te achamos?</h1>
+      <p>Preencha com seus contatos{state.linkedin}</p>
+      <hr />
+      <label>
+        Qual o seu Email?
+        <input type="text" onChange={(e) => EmailChange(e)} />
+      </label>
+      <label>
+        Qual o seu GitHub?
+        <input type="url" onChange={(e) => GitChange(e)} />
+      </label>
+      <label>
+        Qual o seu Linkedin?
+        <input type="url" onChange={(e) => LinkedinChange(e)} />
+      </label>
+      <button onClick={() => navigate("/step02")}>Voltar</button>
+      <button onClick={() => Finish()}>Finalizar</button>
+    </S.Container>
+  );
 };
 
 export default FormStep02;
