5286073d9079c9a03c7a74b2dc2088712dc87859
Manoel Iranilson <manoelvieirafilho0@gmail.com>
2022-07-29 18:08:28 -0300

    Page Step2

--- src/pages/FormStep02/index.tsx
+++ src/pages/FormStep02/index.tsx
@@ -1,9 +1,75 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import SelectOption from "../../components/SelectOption";
+import { FormActions, useForm } from "../../contexts/FormContext";
+import { FaUserAlt, FaUserTie, FaUserGraduate } from "react-icons/fa";
 
 import * as S from "./styles";
 
 const FormStep02: React.FC = () => {
-  return <S.Container>Passo2</S.Container>;
+  const navigate = useNavigate();
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    if (state.name === "") {
+      navigate("/");
+    } else {
+      dispatch({
+        type: FormActions.setCurrentStep,
+        payload: 1,
+      });
+    }
+  });
+
+  function SetLevel(level: number) {
+    dispatch({
+      type: FormActions.setLevel,
+      payload: level,
+    });
+  }
+
+  function NextStep() {
+    navigate("/step03");
+  }
+
+  return (
+    <S.Container>
+      <p>Passo {state.currentStep}/3</p>
+      <h1>{state.name}, o que melhor descreve você?</h1>
+      <p>
+        Escolha a opção que melhor condiz com seu estado atual,
+        profissionalmente
+      </p>
+      <hr />
+
+      <SelectOption
+        title="Sou Iniciante"
+        description="Comecei a programar há menos de 1 ano "
+        icon={<FaUserAlt color="#Fff" size={20} />}
+        selected={state.level === 0}
+        onClick={() => SetLevel(0)}
+      />
+
+      <SelectOption
+        title="Sou intermediario"
+        description="Comecei a programar há menos de 2 anos "
+        icon={<FaUserTie color="#Fff" size={20} />}
+        selected={state.level === 1}
+        onClick={() => SetLevel(1)}
+      />
+
+      <SelectOption
+        title="Sou avançado"
+        description="ja programo há mais de 2 anos "
+        icon={<FaUserGraduate color="#Fff" size={20} />}
+        selected={state.level === 2}
+        onClick={() => SetLevel(2)}
+      />
+
+      <button onClick={() => navigate("/")}>Voltar</button>
+      <button onClick={() => NextStep()}>Proximo</button>
+    </S.Container>
+  );
 };
 
 export default FormStep02;
--- src/pages/FormStep02/styles.ts
+++ src/pages/FormStep02/styles.ts
@@ -1,3 +1,36 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  p {
+    font-size: 13px;
+    color: #b8b8d4;
+  }
+  h1 {
+    margin: 0;
+    padding: 0;
+    font-size: 26px;
+  }
+  hr {
+    height: 1px;
+    border: 0;
+    background-color: #16195c;
+    margin: 30px 0;
+  }
+
+  button {
+    background-color: #25cd89;
+    color: #fff;
+    font-size: 14px;
+    font-weight: bold;
+    padding: 20px 40px;
+    border: 0;
+    border-radius: 30px;
+    cursor: pointer;
+    margin-top: 30px;
+    margin-left: 10px;
+
+    &:hover {
+      background-color: #3cb371;
+    }
+  }
+`;
